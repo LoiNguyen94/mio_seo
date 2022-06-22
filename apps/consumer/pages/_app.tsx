eef68e908be07cae65df73ad1011af467bd9ac2d
LoiNguyen94 <loinh@mioapp.vn>
2022-06-22 11:44:05 +0700

    service worker

--- apps/consumer/pages/_app.tsx
+++ apps/consumer/pages/_app.tsx
@@ -22,6 +22,24 @@ function App({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+          function (registration) {
+            console.log(
+              'Service Worker registration successful with scope: ',
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err);
+          }
+        );
+      });
+    }
+  }, []);
+
+  useEffect(() => {
     async function checkToken() {
       const token = await Storage.get({
         key: 'token',
